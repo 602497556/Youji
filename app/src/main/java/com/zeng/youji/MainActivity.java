3453aa63ab989a9c05ab95e613a7dff93f4efa79
zengjie <zj602497556@qq.com>
2016-07-08 21:01:39 +0800

    给主页添加顶部ToolBar.

--- app/src/main/java/com/zeng/youji/MainActivity.java
+++ app/src/main/java/com/zeng/youji/MainActivity.java
@@ -2,12 +2,36 @@ package com.zeng.youji;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
+
+    /*
+    ToolBar
+     */
+    private Toolbar mToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        mToolBar = (Toolbar) findViewById(R.id.tool_bar);
+        mToolBar.inflateMenu(R.menu.toolbar_menu);
+
+
     }
+
+
+
+
+
+
+
+
+
+
+
+
 }
