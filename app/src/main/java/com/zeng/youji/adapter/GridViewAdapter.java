a8d7845c7e49628a0d8ca1c947f11702cbffa594
zengjie <zj602497556@qq.com>
2016-07-14 16:45:20 +0800

    修改GridView子项的布局.

--- app/src/main/java/com/zeng/youji/adapter/GridViewAdapter.java
+++ app/src/main/java/com/zeng/youji/adapter/GridViewAdapter.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.lidroid.xutils.BitmapUtils;
 import com.lidroid.xutils.bitmap.BitmapCommonUtils;
@@ -62,23 +63,31 @@ public class GridViewAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View convertView, ViewGroup viewGroup) {
+        Area.Destination destination = (Area.Destination) getItem(i);
         ViewHolder holder;
         if(convertView == null){
             holder = new ViewHolder();
             convertView = LayoutInflater.from(mContext).inflate(R.layout.grid_view_item,null,false);
             holder.ivCountry = (ImageView) convertView.findViewById(R.id.iv_country);
+            holder.tvNameCh = (TextView) convertView.findViewById(R.id.tv_name_ch_cn);
+            holder.tvNameEn = (TextView) convertView.findViewById(R.id.tv_name_en);
+            holder.tvNumberPlace = (TextView) convertView.findViewById(R.id.tv_num_place);
             convertView.setTag(holder);
         } else {
             holder = (ViewHolder) convertView.getTag();
         }
-        bitmapUtils.display(holder.ivCountry,destinationList.get(i).getImage_url(),displayConfig);
-
-
+        bitmapUtils.display(holder.ivCountry,destination.getImage_url(),displayConfig);
+        holder.tvNameCh.setText(destination.getName_zh_cn());
+        holder.tvNameEn.setText(destination.getName_en());
+        holder.tvNumberPlace.setText(destination.getPoi_count()+"个旅行地");
         return convertView;
     }
 
-    private class ViewHolder{
+    private class ViewHolder {
         ImageView ivCountry;
+        TextView tvNameCh;
+        TextView tvNameEn;
+        TextView tvNumberPlace;
     }
 
 
